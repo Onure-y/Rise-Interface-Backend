5b302cd91eb11144f58178cb91d903efdf28443e
Onure-y <onuremre555@gmai.com>
2022-06-02 03:36:27 +0300

    folder structure

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const UserModel = require('../models/userModel.js');
+
+exports.getAllUsers = async (req,res) => {
+    const users =  await UserModel.find({});
+
+    try {
+        res.send(users);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+exports.createUser =  (req,res) => {
+    res.status(200).send('sucsess');
+  
+    const newUser = new UserModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email : req.body.email,
+      job: req.body.job
+    })
+  
+    newUser.save().then(() => {
+      console.log('sucsess');
+    }).catch(err => {
+      console.log("Error ! = ",err);
+    })
+  }
